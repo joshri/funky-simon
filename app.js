1b32278a3360e88fafb35b35b20941364b5394db
joshri <joshisrael93@gmail.com>
2020-07-12 17:18:05 -0400

    playerCue

--- app.js
+++ app.js
@@ -9,39 +9,50 @@ round.innerText = `Round: ${roundNumber}`;
 let highScore = document.querySelector('#highScore');
 let highScoreNumber = 0;
 highScore.innerText = `High Score: ${highScoreNumber}`;
+
 //store pattern and player input
 let funkyArray = [];
 let playerArray = [];
 let playerTurn = false;
 
+//pulse timers
+let show = 800;
+let remove = 600;
+
 //enable player input if player turn is true
 window.addEventListener('keydown', playerInput);
-//checker
+
+//incrementer that compares funkyArray and playerArray for every input
 let n = 0;
 
 //pop-up defeat and win notifications
 let reset = document.querySelector('.reset');
 let nextRound = document.querySelector('.nextRound');
 
+//player cue
+let playerCue = document.querySelector('.playerCue');
+
 reset.addEventListener('click', function defeatState() {
 	roundNumber = 0;
 	//remove indicator
-	funkyArray.forEach((num) => {
-		document.getElementById(`${num}`).classList.remove('pulse');
-	});
+	// funkyArray.forEach((num) => {
+	// 	document.getElementById(`${num}`).classList.remove('pulse');
+	// });
 	funkyArray = [];
 	playerArray = [];
-	reset.style.display = 'none';
-	play();
+	reset.classList.add('hide')
+	//reset checker variable
+	n = 0;
+	start.classList.remove('hide');
 });
 
 nextRound.addEventListener('click', function winState() {
 	playerArray = [];
-	nextRound.style.display = 'none';
+	nextRound.classList.add('hide');
 	//remove indicator
-	funkyArray.forEach((num) => {
-		document.getElementById(`${num}`).classList.remove('pulse');
-	});
+	// funkyArray.forEach((num) => {
+	// 	document.getElementById(`${num}`).classList.remove('pulse');
+	// });
 	//update High Score
 	if (roundNumber > highScoreNumber) {
 		highScore.innerText = `High Score ${roundNumber}`;
@@ -54,7 +65,7 @@ nextRound.addEventListener('click', function winState() {
 //start button
 let start = document.querySelector('.start');
 start.addEventListener('click', () => {
-	start.style.display = 'none';
+	start.classList.add('hide');
 	play();
 });
 
@@ -64,18 +75,22 @@ start.addEventListener('click', () => {
 function funkyChoice() {
 	let choice = Math.floor(Math.random() * 5 + 1);
 	let choiceString = choice.toString();
-	while (funkyArray.length < roundNumber) {
-		funkyArray.push(choiceString);
-	}
+	funkyArray.push(choiceString);
 }
 
-//indicate funky choice for each choice in array (pulse for .5 second?), then set playerTurn to true
-function displayFunk() {
-	funkyArray.forEach((num, index) => {
-        document.getElementById(`${num}`).classList.add('pulse');
-		setTimeout( () => document.getElementById(`${num}`).classList.remove('pulse'), 1000);
+function displayFunk(funkyArray, i = 0) {
+	if (i === funkyArray.length) {
+        playerCue.classList.remove('hide');
+        setTimeout(() => playerCue.classList.add('hide'), remove);
 		playerTurn = true;
-	});
+		return;
+	}
+	let num = funkyArray[i];
+	document.getElementById(`${num}`).classList.add('pulse');
+	setTimeout(() => {
+		document.getElementById(`${num}`).classList.remove('pulse');
+		setTimeout(() => displayFunk(funkyArray, i + 1), remove);
+	}, show);
 }
 
 //play
@@ -83,27 +98,25 @@ function play() {
 	roundNumber++;
 	round.innerText = `Round: ${roundNumber}`;
 	funkyChoice();
-	displayFunk();
+	displayFunk(funkyArray);
 }
-
 
 function playerInput(event) {
 	if (playerTurn) {
-        const keys = ['1', '2', '3', '4', '5']
-        //variable for event key
-		if (keys.indexOf(event.key) > -1) {
+		const keys = ['1', '2', '3', '4', '5'];
+		//variable for event key
+		if (keys.indexOf(event.key) !== -1) {
 			playerArray.push(event.key);
 			//check for loss every input
-
 			if (playerArray[n] !== funkyArray[n]) {
 				//defeat state button
-				reset.style.display = 'block';
+				reset.classList.remove('hide');
 				return;
 			} else {
 				//win round if you get through entire funky array
 				n++;
 				if (n > funkyArray.length - 1) {
-					nextRound.style.display = 'block';
+					nextRound.classList.remove('hide');
 					playerTurn = false;
 					return;
 				}
